0bb54b6e57f5d22a07fe619dc6e0120c4d08650a
papadkostas <kostas.papadopulos@gmail.com>
2014-05-04 01:03:40 +0300

    Incomplete function
    Init function added

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "main.h"
-int status;
+int statusinit,statusread;
+char buf[5];
+void BH1750_Init();
 int BH1750_Read();	//Function Prototype
 void Delay(volatile uint32_t nCount);
 int main(){
@@ -14,34 +16,49 @@ int main(){
   UB_LCD_Rotate_0();
   UB_Font_DrawString(5,10,"STM32F429-BH1750 TEST",&Arial_11x18,RGB_COL_BLUE,RGB_COL_GREEN);
   UB_I2C3_Init();
-  UB_I2C3_WriteByte(0x23<<1,0x00,0x10);
-  /*Set resolution data 0x10 at slave address 0x23<<1
-  * page 5 @ http://www.dfrobot.com/image/data/SEN0097/BH1750FVI.pdf
-  */
+  BH1750_Init();
+
   while(1){
 	  int lux;
 	  char buf[5];
 	  lux = BH1750_Read();
 	  if(lux >= 0){
 		  sprintf(buf,"Lux = %d",lux);
-		  UB_Font_DrawString(10,40,buf,&Arial_11x18,RGB_COL_BLUE,RGB_COL_GREEN);
+		  UB_Font_DrawString(10,60,buf,&Arial_11x18,RGB_COL_BLUE,RGB_COL_GREEN);
 	  }
 	  else
-		  sprintf(buf,"Status = %d",status);
-		  UB_Font_DrawString(10,40,"FAILED!",&Arial_11x18,RGB_COL_RED,RGB_COL_GREEN);
+		  sprintf(buf,"Status = %d",statusread);
 	  	  UB_Font_DrawString(10,60,buf,&Arial_11x18,RGB_COL_RED,RGB_COL_GREEN);
-	  UB_Led_Toggle(LED_GREEN);
 	  Delay(50000000);
   }
+}
 
+void BH1750_Init(){
+	/*Set resolution data 0x10 at slave address 0x23<<1
+	* page 5 @ http://www.dfrobot.com/image/data/SEN0097/BH1750FVI.pdf
+	*/
+	statusinit = UB_I2C3_WriteByte(0x23<<1,0x00,0x10);
+	if(statusinit == 0){
+		UB_Font_DrawString(10,40,"Init Ok!",&Arial_11x18,RGB_COL_BLUE,RGB_COL_GREEN);
+	}
+	else {
+		sprintf(buf,"Init Failed : %d", statusinit);
+		UB_Font_DrawString(10,40,buf,&Arial_11x18,RGB_COL_RED,RGB_COL_GREEN);
+	}
 }
 
 int BH1750_Read(){
+	/* Do not read if init failed before */
+	if(statusinit < 0){
+		UB_Led_Toggle(LED_RED);
+		statusread = -10;
+		return statusread;
+	}
 	UB_Led_Toggle(LED_GREEN);
 	uint16_t val;
 	/* Check if any error occur from I2C communication */
-	status = UB_I2C3_ReadMultiByte(0x23<<1,0x00,2);
-	if(status == 0){
+	statusread = UB_I2C3_ReadMultiByte(0x23<<1,0x00,2);
+	if(statusread == 0){
 		/* Luminous Calculation */
 		val=((I2C3_DATA[0]<<8)|I2C3_DATA[1])/1.2;
 		/* Return calculated luminosity */
@@ -52,7 +69,7 @@ int BH1750_Read(){
 		return -1;
 }
 
-void Delay(volatile uint32_t nCount) {
+void Delay(volatile uint32_t nCount){
   while(nCount--) {
   }
 }
